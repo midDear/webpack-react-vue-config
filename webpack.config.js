659abbeb36377402887677aa3545a0b2c20653d3
shaokun <605416017@qq.com>
2019-07-23 21:41:20 +0800

    仅转义js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader"
+               
+            }
+        ]
+    }
+};
